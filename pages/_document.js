bab1094c71165b1b792c706a8d909ad59f50a22f
Reid Roman <reid@reidroman.com>
2018-10-06 13:09:08 -0700

    ant design

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+import Document, { Head, Main, NextScript } from 'next/document'
+import stylesheet from 'antd/dist/antd.min.css'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+          <style>{`html, body { height: 100%; }`}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
-export default () => < div > Welcome to next.js! < /div>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DatePicker } from 'antd';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+        <div style={{ margin: 100 }}>
+        <h1>AntDesign Demo</h1>
+        <hr /><br />
+        <DatePicker />
+        </div>
+    );
+  }
+}
